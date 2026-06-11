2210121528a55bd5485c4015b678df879b9f5cb2
Zixi-Cici <zwang4234@student.unimelb.edu.au>
2026-06-12 01:03:48 +1000

    home page & product list page & product detail page responsive

---
+++ Javascript/components.js
@@ -0,0 +1,11 @@
+const menuOverlay = document.getElementById("menuOverlay")
+const closeMenuButton = document.getElementById("closeButton")
+const menuButtons = document.getElementById("menuButton")
+
+menuButtons.addEventListener("click", function(){
+    menuOverlay.classList.add("show")
+})
+
+closeMenuButton.addEventListener("click", function(){ //close menu button
+    menuOverlay.classList.remove("show")
+})
